c099079e8c22acd5dd0e05ef1bd1183380beeaf2
Samir Antonio <ext.samir.antonio@farfetch.com>
2023-09-24 13:12:58 +0100

    init

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -1,71 +1,27 @@
 'use client'
 
-export default function Page() {
-  const [account, setAccount] = useState('');
-  const [contract, setContract] = useState(null);
-  const [mood, setMood] = useState('');
+import ChainBridge from './ChainBridge';
+import About from './about';
+import { useState } from "react";
+import React from 'react';
 
-  useEffect(() => {
-    if (window.ethereum) {
-      window.ethereum.on('accountsChanged', accounts => {
-        setAccount(accounts[0]);
-      });
-    }
-  }, []);
-
-  async function connectWallet() {
-    const providerOptions = {
-      walletconnect: {
-        package: WalletConnectProvider,
-        options: {
-          rpc: {
-            31337: 'http://127.0.0.1:8545/'
-          }
-        },
-      },
-    };
-  
-    const web3Modal = new Web3Modal({
-      network: 'LocalGnc',
-      cacheProvider: true,
-      providerOptions,
-    });
-
-    const provider = await web3Modal.connect();
-    const web3Provider = new ethers.providers.Web3Provider(provider);
-    const signer = web3Provider.getSigner();
-    const yourContract = new ethers.Contract(CONTRACT_ADDRESS, abi, signer);
-
-    setAccount(await signer.getAddress());
-    setContract(yourContract);
-  }
-
-  async function updateMood() {
-    await contract.setMood(mood);
-  }
-
-  async function fetchMood() {
-    const mood = await contract.getMood();
-    alert(`Mood is: ${mood}`);
-  }
+export default function App() {
+  const [state, setState] = useState("home");
 
   return (
     <div>
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/contract">Contract</Link></li>
-        </ul>
-      </nav>
+        <nav>
+            <ul>
+                <li><a onClick={() => setState('home')}>Home</a></li>
+                <li><a onClick={() => setState('about')}>About</a></li>
+                <li><a onClick={() => setState('chainbridge')}>Chain Bridge</a></li>
+            </ul>
+        </nav>
 
-      <button onClick={connectWallet}>Connect Wallet</button>
-      {account && contract && (
-        <>
-          <input type="text" value={mood} onChange={e => setMood(e.target.value)} />
-          <button onClick={updateMood}>Update Mood</button>
-          <button onClick={fetchMood}>Fetch Mood</button>
-        </>
-      )}
+        <p>Current state: {state}</p>
+
+        {state === 'about' && <About />}
+        {state === 'chainbridge' && <ChainBridge />}
     </div>
   );
 }
