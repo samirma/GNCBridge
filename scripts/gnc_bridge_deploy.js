9880fc27108cfb1a3d2385feef3f211d5fb523d8
Samir Antonio <ext.samir.antonio@farfetch.com>
2023-10-24 20:20:02 +0100

    gnc_bridge_deploy

--- scripts/gnc_bridge_deploy.js
+++ scripts/gnc_bridge_deploy.js
@@ -1,12 +1,28 @@
 const hre = require("hardhat");
+const fs = require('fs');
+const path = require('path');
 
 async function main() {
   
-  const lock = await hre.ethers.deployContract("GncBridge");
+  let addressWallet = "0x125089C0403C4Cd3a01c18e6FE1D46Ab9bB34344";
+  const lock = await hre.ethers.deployContract("GncBridge", [addressWallet]);
 
   await lock.waitForDeployment();
 
   console.log(`GncBridge deployed to ${lock.target}`  );
+
+  const contractsDir = path.join(__dirname, '..', 'my-app', 'constants');
+  
+  if (!fs.existsSync(contractsDir)) {
+    fs.mkdirSync(contractsDir);
+  }
+
+  fs.writeFileSync(
+    path.join(contractsDir, 'gncBridge.js'),
+    `export const BRIDGE_ADDRESS = "${lock.target}";\n` +
+    `export const ABI_BRIDGE = ${JSON.stringify(lock.interface.format('json'), null, 2)};\n`
+  );
+
 }
 
 // We recommend this pattern to be able to use async/await everywhere
