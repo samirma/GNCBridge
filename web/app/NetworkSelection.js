45449218f1e28a34f2a8f396d0706883acc68a17
Samir Antonio <samir.ma@gmail.com>
2024-10-20 12:27:54 +0100

    improve network selection

--- web/app/NetworkSelection.js
+++ web/app/NetworkSelection.js
@@ -4,75 +4,68 @@ import { getNetworkConfig } from 'shared/constants/networks';
 import { GNC, CHAIN } from 'shared/constants/env';
 
 export default function NetworkSelection({ onNetworkSelect }) {
-
     const [gnc, setGnc] = useState('');
     const [chain, setChain] = useState('');
+    const networks = [
+        { id: GNC, name: gnc, img: 'img/chain-1.png' },
+        { id: CHAIN, name: chain, img: 'img/chain-137.png' }
+    ];
+    const [selectedOrigin, setSelectedOrigin] = useState(networks[0]);
+    const [selectedTarget, setSelectedTarget] = useState(networks[1]);
 
     useEffect(() => {
         setGnc(getNetworkConfig(GNC).chainName);
         setChain(getNetworkConfig(CHAIN).chainName);
-      }, []);
+    }, []);
 
-    const selectNetWork = (value) => {
-        console.error('value:', value);
-        onNetworkSelect(value);
+    const selectNetworkOrigin = (id) => {
+        const selectedNetwork = networks.find(network => network.id === id);
+        if (selectedNetwork) {
+            setSelectedOrigin(selectedNetwork);
+            onNetworkSelect(selectedNetwork);
+        }
     };
+
+    const selectNetworkTarget = (id) => {
+        const selectedNetwork = networks.find(network => network.id === id);
+        if (selectedNetwork) {
+            setSelectedTarget(selectedNetwork);
+            onNetworkSelect(selectedNetwork);
+        }
+    };
+
+    const renderNetworkItem = (name, img, action) => (
+        <a className="dropdown-item" href="#" onClick={action}>
+            <div className="network_img">
+                <img src={img} alt={name} />
+            </div>
+            <span>{name}</span>
+        </a>
+    );
+
+    const renderDropdown = (selected, onSelect, label) => (
+        <div className="dropdown">
+            <button className="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">
+                {renderNetworkItem(selected.name, selected.img, null)}
+                <span>{selected.name}</span>
+            </button>
+            <div className="dropdown-menu">
+                {networks.map(network =>
+                    renderNetworkItem(network.name, network.img, () => onSelect(network.id))
+                )}
+            </div>
+        </div>
+    );
 
     return (
         <div className="row">
-        <div className="col-md-5">
-            <div className="dropdown">
-            <button className="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown">
-                <a className="dropdown-item" href="#">
-                <div className="network_img">
-                    <img src="img/chain-1.png" alt="Chain 1" />
-                </div>
-                <span>{chain}</span>
-                </a>
-            </button>
-            <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                <a className="dropdown-item" href="#" onClick={() => selectNetWork(GNC)}>
-                <div className="network_img">
-                    <img src="img/chain-1.png" alt="Chain 1" />
-                </div>
-                <span>{chain}</span>
-                </a>
-                <a className="dropdown-item" href="#" onClick={() => selectNetWork(CHAIN)}>
-                <div className="network_img">
-                    <img src="img/chain-137.png" alt="Chain 137" />
-                </div>
-                <span>{gnc}</span>
-                </a>
+            <div className="col-md-5">
+                {renderDropdown(selectedOrigin, selectNetworkOrigin, 'Origin')}
             </div>
+            <div className="col-md-2"></div>
+            <div className="col-md-5">
+                {renderDropdown(selectedTarget, selectNetworkTarget, 'Target')}
             </div>
-        </div>
-        <div className="col-md-2"></div>
-        <div className="col-md-5">
-            <div className="dropdown">
-            <button className="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown">
-                <a className="dropdown-item" href="#">
-                <div className="network_img">
-                    <img src="img/chain-1.png" alt="Chain 1" />
-                </div>
-                <span>{gnc}</span>
-                </a>
-            </button>
-            <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                <a className="dropdown-item" href="#" onClick={() => selectNetWork(GNC)}>
-                <div className="network_img">
-                    <img src="img/chain-1.png" alt="Chain 1" />
-                </div>
-                <span>{chain}</span>
-                </a>
-                <a className="dropdown-item" href="#" onClick={() => selectNetWork(CHAIN)}>
-                <div className="network_img">
-                    <img src="img/chain-137.png" alt="Chain 137" />
-                </div>
-                <span>{gnc}</span>
-                </a>
-            </div>
-            </div>
-        </div>
         </div>
     );
 }
